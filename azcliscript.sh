f5e526ae3f3d0ad65f0587ae8f97e398f3405ddc
Yunge Zhu <yungez@microsoft.com>
2022-03-03 09:49:33 +0800

    Update azcliscript.sh

--- azcliscript.sh
+++ azcliscript.sh
@@ -1,1 +1,1 @@
-az rest --method PUT --uri $WEBAPP_ID --body \{\"name\":\"$CONN_NAME\",\"properties\":\{\"clientType\":\"$CLIENT_TYPE\",\"targetId\":\"$KEYVAULT_ID\",\"authInfo\":\{\"authType\":"$AUTH_TYPE"\}\}\} --headers x-ms-serviceconnector-user-token=$ACCESS_TOKEN
+az rest --method PUT --uri $WEBAPP_ID --body {\"name\":\"$CONN_NAME\",\"properties\":\{\"clientType\":\"$CLIENT_TYPE\",\"targetId\":\"$KEYVAULT_ID\",\"authInfo\":\{\"authType\":"$AUTH_TYPE"}}} --headers x-ms-serviceconnector-user-token=$ACCESS_TOKEN
